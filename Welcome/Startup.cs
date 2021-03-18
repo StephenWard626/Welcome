f71ebd3d8b131c677864becace71ad5cfd5f1f83
StephenWard626 <78171486+StephenWard626@users.noreply.github.com>
2021-03-18 19:16:48 +0000

    added Date Time code

--- Welcome/Startup.cs
+++ Welcome/Startup.cs
@@ -50,7 +50,7 @@ namespace Welcome
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Welcome}/{action=Index}/{id?}");
             });
         }
     }
